0e711e937b34c413b0a1670d49c39796c0321785
c4rl2s0n <57667488+c4rl2s0n@users.noreply.github.com>
2025-08-01 14:48:57 +0200

    added missing file

---
+++ lib/common/widgets/misc/vpn_launcher.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:netguard/common/common.dart';
+import 'package:netguard/features/session_logs/session_logs.dart';
+
+class VpnLauncher extends StatelessWidget {
+  const VpnLauncher(this.buildChild, {super.key});
+
+  final Widget Function(Future Function(BuildContext context) launchVpn)
+  buildChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return buildChild(_launchVpn);
+  }
+
+  Future _launchVpn(BuildContext context) async {
+    if (settingsCubit.state.observeOnly &&
+        await ConfirmationDialog.ask(
+          context,
+          title: "Observation-Mode enabled",
+          content:
+              "In observation mode, the traffic will NOT be filtered! Do you want to turn observation mode OFF for real protection?",
+          declineText: "No",
+          confirmText: "Turn OFF",
+        )) {
+      settingsCubit.setObserveOnly(false);
+    }
+    await sessionCubit.startVpn();
+    if (settingsCubit.state.logTraffic && context.mounted) {
+      context.navigator.navigateAndClearRoute(const SessionLogs());
+    }
+  }
+}
